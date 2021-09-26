4f417de89f590c320586d29a9ee357dba3fff93b
zalphee <zlaphee@gmail.com>
2021-09-26 16:07:53 +0900

    기본 lecture_list 제작

--- inflearn_clone_prj/inflearn_lecture/urls.py
+++ inflearn_clone_prj/inflearn_lecture/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_list, name="home_list")
+    path('', views.home_list, name="home_list"),
+    path('lecture_list/', views.lecture_list, name="lecture_list"),
 ]
--- inflearn_clone_prj/inflearn_lecture/views.py
+++ inflearn_clone_prj/inflearn_lecture/views.py
@@ -8,3 +8,9 @@ def home_list(request):
     return render(request, 'inflearn_lecture/home_list.html', {
         'texts':texts
     })
+
+def lecture_list(request):
+    texts = myText.objects.filter()
+    return render(request, 'inflearn_lecture/lecture_list.html', {
+        'texts':texts
+    })
