bc6109106ef72bbaf40e0180f104d1e58e1b23e0
zalphee <zlaphee@gmail.com>
2021-09-26 11:38:29 +0900

    myText 모델 추가 및 어드민에 글등록

--- inflearn_clone_prj/inflearn_lecture/models.py
+++ inflearn_clone_prj/inflearn_lecture/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class myText(models.Model):
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=200)
+    img_url = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+    
+    def publish(self):
+        self.save()
