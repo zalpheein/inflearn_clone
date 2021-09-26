d45fb531bd6ac401ec2ca8ea2c38a0f62ee60af3
zalphee <zlaphee@gmail.com>
2021-09-26 15:03:20 +0900

    메인에 이미지 + 게시글 순차 노출

--- inflearn_clone_prj/inflearn_lecture/views.py
+++ inflearn_clone_prj/inflearn_lecture/views.py
@@ -3,7 +3,8 @@ from .models import myText
 
 def home_list(request):
 
-    texts = myText.objects.filter(category='HTML')
+    #texts = myText.objects.filter(category='HTML')
+    texts = myText.objects.filter()
     return render(request, 'inflearn_lecture/home_list.html', {
         'texts':texts
     })
